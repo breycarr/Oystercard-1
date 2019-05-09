4d305e26b3c69bc2442bb024d65dae780b7e32cb
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-09 16:25:38 +0100

    Challenge 12, refactoring Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com> Co-authored-by: Ollie Brownlow <olliebrownlow@gmail.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,9 @@ describe Oystercard do
   let(:topped_up_card) { Oystercard.new.tap { |card| card.top_up(10)} }
   let(:start_station) { double(:station) }
   let(:end_station) { double(:station) }
-  let(:journey) { { start_station: start_station, end_station: end_station } }
+  let(:complete_journey_card) { Oystercard.new.tap { |card| card.top_up(5)
+                                                            card.touch_in(start_station)
+                                                            card.touch_out(end_station) } }
 
   it 'has 0 balance by default' do
     expect(subject.balance).to eq(0)
@@ -38,9 +40,7 @@ describe Oystercard do
   describe '#touch_out' do
 
     it 'should end a journey with #touch_out' do
-      topped_up_card.touch_in(start_station)
-      topped_up_card.touch_out(end_station)
-      expect(topped_up_card.in_journey?).to eq(false)
+      expect(complete_journey_card.in_journey?).to eq(false)
     end
 
     it "should deduct a fare at the end of the journey" do
@@ -51,25 +51,16 @@ describe Oystercard do
   end
 
   describe "#journey_history" do
+    let(:journey) { { start_station: start_station, end_station: end_station } }
+
     it "should generate an empty list of journeys by default" do
       expect(subject.journeys).to be_empty
     end
     it "should return the history of a journey" do
-      topped_up_card.touch_in(start_station)
-      topped_up_card.touch_out(end_station)
-      expect(topped_up_card.journey_history).to eq("Journey 1: #{start_station} to #{end_station}")
+      expect(complete_journey_card.journey_history).to eq("Journey 1: #{start_station} to #{end_station}")
     end
     it "stores a journey" do
-      topped_up_card.touch_in(start_station)
-      topped_up_card.touch_out(end_station)
-      expect(topped_up_card.journeys).to include journey
+      expect(complete_journey_card.journeys).to include journey
     end
-    # it "should return all previous journeys" do
-    #   topped_up_card.touch_in(start_station)
-    #   topped_up_card.touch_out(end_station)
-    #   topped_up_card.touch_in(start_station)
-    #   topped_up_card.touch_out(end_station)
-    #   expect(topped_up_card.journey_history).to eq("Journey 1: #{start_station} to #{end_station}\nJourney 2: #{start_station} to #{end_station}")
-    # end
   end
 end
